c4537fc890fcb3aebb394d1d5763d8c138948d20
rayanMELZI <rayanmelzi@outlook.com>
2024-07-31 00:30:52 +0100

    added an annimation to the side-bar on hover before opening it , and fixed the border-radius of the navBar

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -5,6 +5,21 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 function SideMenu() {
   const [isSideMenuOpen, setIsSideMenuOpen] = React.useState(false);
 
+  let timeOut;
+
+  function handleMouseEnter() {
+    const sideBarEl = document.querySelector(".side-menu");
+    sideBarEl.style.width = "10px";
+    timeOut = setTimeout(() => {
+      sideBarEl.style.width = "0px";
+    }, 1000);
+  }
+  function handleMouseLeave() {
+    const sideBarEl = document.querySelector(".side-menu");
+    clearTimeout(timeOut);
+    sideBarEl.style.width = "0px";
+  }
+
   return (
     <>
       <div
@@ -25,7 +40,12 @@ function SideMenu() {
             <Bars3Icon
               className="h-6 w-6"
               strokeWidth={2}
-              onClick={() => setIsSideMenuOpen(true)}
+              onClick={() => {
+                setIsSideMenuOpen(true);
+                clearTimeout(timeOut);
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             />
           )}
         </button>
